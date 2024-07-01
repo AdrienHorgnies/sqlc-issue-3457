7bad49ef9cb9b43b2bf62ddbc2ddfa1ee6beee66
Adrien Horgnies <ah@fita.dev>
2024-07-01 13:27:10 +0200

    Make logs clearer

--- main.go
+++ main.go
@@ -16,25 +16,28 @@ func main() {
 	log.Println("Opening database")
 	db, dbErr := sql.Open("sqlite3", "main.db")
 	if dbErr != nil {
-		log.Fatal("Cannot open/create database", dbErr)
+		log.Fatalln("Cannot open/create database", dbErr)
 	}
 	q := New(db)
 	ctx := context.Background()
 
 	log.Println("Creating tables")
-	if _, tablesErr := db.ExecContext(ctx, ddl); tablesErr != nil {
-		log.Fatal("Cannot create tables", tablesErr)
+	_, tablesErr := db.ExecContext(ctx, ddl)
+	if tablesErr != nil {
+		log.Fatalln("Cannot create tables:", tablesErr)
 	}
 
 	log.Println("Inserting users")
 	insertErr := q.InsertUsers(ctx)
 	if insertErr != nil {
-		log.Fatal("Cannot create users", insertErr)
+		log.Fatalln("Cannot create users:", insertErr)
 	}
+
 	log.Println("Updating users")
 	updateErr := q.UpdateUsers(ctx, "bar")
 	if updateErr != nil {
-		log.Fatal("Cannot update users", updateErr)
+		log.Fatalln("Cannot update users:", updateErr)
 	}
+
 	log.Println("Done")
 }
